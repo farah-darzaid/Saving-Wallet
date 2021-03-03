62d57e1fbbbd3a5c3217cedef5cb51d6518126e4
farah <account@peak.jo>
2021-03-03 20:46:31 +0200

    Submit Commit

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -20,7 +20,9 @@ class UserSeeder extends Seeder
                 'phone' => '0790799797',
                 'birthday' => '12-12-2020',
                 'password' => Hash::make('user12345'),
-                'avatar' => 'uploads/9rBkrjMQ4c5iXoQkrX881yyMYQ9oQ6WUCUxVhBqr.png'
+                'avatar' => 'uploads/9rBkrjMQ4c5iXoQkrX881yyMYQ9oQ6WUCUxVhBqr.png',
+                'role' => 'user',
+                'created_at' => '2021-03-02 20:02:04'
 
             ],
             [
@@ -29,7 +31,10 @@ class UserSeeder extends Seeder
                 'phone' => '0780788787',
                 'birthday' => '1-1-2020',
                 'password' => Hash::make('admin12345'),
-                'avatar' => 'uploads/9rBkrjMQ4c5iXoQkrX881yyMYQ9oQ6WUCUxVhBqr.png'
+                'avatar' => 'uploads/9rBkrjMQ4c5iXoQkrX881yyMYQ9oQ6WUCUxVhBqr.png',
+                'role' => 'admin',
+                'created_at' => '2021-03-02 20:02:04'
+
             ]
         ]);
     }
